d908e65f165a2ccc11ad89a5a3537cd32e410612
doc_bacardi <devnull@localhost>
2012-05-02 09:43:29 +0000

    fix include path for assembler defines

--- src/netx_regdef.s
+++ src/netx_regdef.s
@@ -4,16 +4,16 @@
 
 #if ASIC_TYP==500
 #       define HOST NX500
-#       include "netx500_regdef.s"
+#       include "netx500/netx500_regdef.s"
 #elif ASIC_TYP==56
 #       define HOST NX56
-#       include "netx56_regdef.s"
+#       include "netx56/netx56_regdef.s"
 #elif ASIC_TYP==50
 #       define HOST NX50
-#       include "netx50_regdef.s"
+#       include "netx50/netx50_regdef.s"
 #elif ASIC_TYP==10
 #       define HOST NX10
-#       include "netx10_regdef.s"
+#       include "netx10/netx10_regdef.s"
 #else
 #       error "no host define set!"
 #endif
