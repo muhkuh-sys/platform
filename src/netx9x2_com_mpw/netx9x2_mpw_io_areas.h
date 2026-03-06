a7245bd4f2a9e32663a4b9542e92d51ecfaf080b
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2026-03-06 13:43:14 +0100

    Add the Sqi and ComXpsi areas.

--- src/netx9x2_com_mpw/netx9x2_mpw_io_areas.h
+++ src/netx9x2_com_mpw/netx9x2_mpw_io_areas.h
@@ -10,6 +10,7 @@
 #define NX9X2_MPW_DEF_ptAsicCtrlArea NX9X2_MPW_COM_ASIC_CTRL_AREA_T * const ptAsicCtrlArea = (NX9X2_MPW_COM_ASIC_CTRL_AREA_T * const)(Addr_NX9X2_MPW_com_asic_ctrl);
 #define NX9X2_MPW_DEF_ptGlobalAsicCtrlArea NX9X2_MPW_GLOBAL_ASIC_CTRL_AREA_T * const ptGlobalAsicCtrlArea = (NX9X2_MPW_GLOBAL_ASIC_CTRL_AREA_T * const)(Addr_NX9X2_MPW_global_asic_ctrl);
 #define NX9X2_MPW_DEF_ptComAdaTimerArea NX9X2_MPW_RV_TIMER_AREA_T * const ptComAdaTimerArea = (NX9X2_MPW_RV_TIMER_AREA_T * const)(Addr_NX9X2_MPW_com_ada_timer);
-
+#define NX9X2_MPW_DEF_ptSqiArea NX9X2_MPW_SQI_AREA_T * const ptSqiArea = (NX9X2_MPW_SQI_AREA_T * const)(Addr_NX9X2_MPW_sqi);
+#define NX9X2_MPW_DEF_ptComXspiArea NX9X2_MPW_XSPI_AREA_T * const ptComXspiArea = (NX9X2_MPW_XSPI_AREA_T * const)(Addr_NX9X2_MPW_com_xspi);
 
 #endif  /* __NETX9X2_MPW_COM_IO_AREAS_H__ */
